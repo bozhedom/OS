7ff912e34fb1a7684360ef2d59ae1aad3eca2b2f
Daniil <c.cffc@mail.ru>
2025-01-20 06:19:33 +1000

    Bit change for build Linux

--- lab1/build.sh
+++ lab1/build.sh
@@ -4,6 +4,11 @@ echo "Updating project from Git..."
 
 git pull origin master
 
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to update project from Git."
+    exit 1
+fi
+
 echo "Building project..."
 
 mkdir -p build
@@ -12,6 +17,16 @@ cd build
 
 cmake ..
 
+if [ $? -ne 0 ]; then
+    echo "Error: CMake configuration failed."
+    exit 1
+fi
+
 make
+
+if [ $? -ne 0 ]; then
+    echo "Error: Build failed."
+    exit 1
+fi
 
 echo "Build complete!"
